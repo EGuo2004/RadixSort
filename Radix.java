13665699640948a13c402dbe2cb368ea95d20118
unknown <>
2021-01-13 00:13:28 -0500

    Adds length

--- Radix.java
+++ Radix.java
@@ -1,10 +1,17 @@
 public class Radix{
   public static int nth(int n, int col) {
+    if(col > length(n)) {
+      throw new IndexOutOfBoundsException("Index Out Of Bounds");
+    }
     if (col == 0) {
       return (n % 10);
     } else {
       int value = (n % Math.pow(10, (col + 1))) - (n % Math.pow(10, (col)));
       return value/(Math.pow(10, col));
     }
+  }
+
+  public static int length(int n) {
+    return Math.ceil(Math.log10(n));
   }
 }
